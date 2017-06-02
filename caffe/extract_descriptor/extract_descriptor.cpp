9a7426ee6d90ccd904378ee77c5b92382a36f7ad
Dongmin.Cho <dongmin.cho@ajou.ac.kr>
2017-06-02 22:57:04 +0900

    remove crop procses

--- caffe/extract_descriptor/extract_descriptor.cpp
+++ caffe/extract_descriptor/extract_descriptor.cpp
@@ -47,53 +47,7 @@ int extract_feature(string in_file_path, string out_file_path)
     int ndim = 4096;
     int nimsz = 100;
     cv::Mat faceimg = cv::imread(in_file);
-    
-    frontal_face_detector detector = get_frontal_face_detector();
-    shape_predictor sp;
-    deserialize("../../../face_recognition/src/shape_predictor_68_face_landmarks.dat") >> sp;
 
-    array2d<rgb_pixel> img;
-	assign_image(img, cv_image<rgb_pixel>(faceimg));
-    //load_image(img, in_file_path); 
-    
-    pyramid_up(img);
-
-    std::vector<dlib::rectangle> dets = detector(img);
-
-    std::vector<full_object_detection> shapes;
-    std::vector<dlib::rectangle> rect;
-	
-	if(dets.size() == 0)
-	{
-		unlink(in_file.c_str());		
-		return -1;
-	}
-    for (unsigned long j = 0; j < dets.size(); ++j)
-    {
-        full_object_detection shape = sp(img, dets[j]);
-        shapes.push_back(shape);
-    }
-
-	for (size_t k = 0; k < shapes.size(); k++)
-	{
-		float sum_left_x = 0;
-		float sum_left_y = 0;
-		for(size_t left = 36; left <= 41; left ++)
-		{
-			sum_left_x += shapes[k].part(left).x();
-			sum_left_y += shapes[k].part(left).y();
-		}
-		float sum_right_x = 0;
-		float sum_right_y = 0;
-		for(size_t right = 42; right <= 47; right ++)
-		{
-			sum_right_x += shapes[k].part(right).x();
-			sum_right_y += shapes[k].part(right).y();
-		}
-		cv::Point2f eye_loc_L(sum_left_x/6, sum_left_y/6), eye_loc_R(sum_right_x/6, sum_right_y/6);
-		cv::Mat input;
-		faceimg = toMat(img);
-		faceimg = _cropFaceImagebyEYE(faceimg, eye_loc_L, eye_loc_R, nimsz, nimsz, 0.4f, 0.38f);
 		float *blob = NULL;
 		blob = classifier.Extract_Feature(faceimg, blobname, ndim, ndepth, 1, false);
 		in_file = in_file.substr(in_file.find_last_of("/")+1,in_file.find_last_of(".")-in_file.find_last_of("/")-1); //remove all except file name
@@ -110,7 +64,7 @@ int extract_feature(string in_file_path, string out_file_path)
 	}
 		return 1;
 }
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {//argv[1] = out put folder, argv[2] = input file path *.jpg
 
 	if(argc == 1)
